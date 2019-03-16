8c0d48993d78c692ca475b924b6096157c59601a
ParthibanSamuvel <parthiban.s@skava.com>
2019-03-16 14:21:14 +0530

    Two class added for notification

---
+++ src/main/java/com/data/classifier/model/MailEntity.java
@@ -0,0 +1,17 @@
+package com.data.classifier.model;
+
+public class MailEntity
+{
+    private String email;
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    
+}
---
+++ src/main/java/com/data/classifier/model/OtpEntity.java
@@ -0,0 +1,17 @@
+package com.data.classifier.model;
+
+public class OtpEntity
+{
+    private String otp;
+
+    public String getOtp()
+    {
+        return otp;
+    }
+
+    public void setOtp(String otp)
+    {
+        this.otp = otp;
+    }
+    
+}
